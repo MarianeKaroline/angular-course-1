2969a6e4b780716690851970039bcda2abda9ea8
marianekarolines <nanikaroline292@gmail.com>
2024-07-01 21:34:23 -0300

    criando course-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import {COURSES} from '../db-data';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
